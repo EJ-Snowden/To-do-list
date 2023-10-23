7a25d775f2c6d9474507decc94424f9b88dfea3d
EJ-Snowden <shulgindenis2@gmail.com>
2023-10-24 00:10:45 +0200

    RecyclerView commit

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton = findViewById<Button>(R.id.expand_button1)
+        var isButtonPressed = false
+
+        myButton.setOnClickListener {
+            if (isButtonPressed) {
+                myButton.foreground = ContextCompat.getDrawable(this, R.drawable.ic_expand_more )
+            } else {
+                myButton.foreground = ContextCompat.getDrawable(this, R.drawable.ic_expand_less)
+            }
+            isButtonPressed = !isButtonPressed
+        }
     }
 }
